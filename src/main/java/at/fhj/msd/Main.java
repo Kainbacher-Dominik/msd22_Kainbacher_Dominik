0971a716e24e9c4b77cd6c0e1004429f749c6ff6
Dominik Kainbacher <dominik.kainbacher@edu.fh-joanneum.at>
2023-06-01 10:39:17 +0200

    updated pom.xml and src

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,6 +1,13 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
 public class Main {
+
+    private static Logger logger=LogManager.getLogger();
     public static void main(String[] args) {
         double num1= Double.parseDouble(args[0]);
         double num2= Double.parseDouble(args[1]);
@@ -8,5 +15,9 @@ public class Main {
         System.out.println(Calculator.minus(num1,num2));
         System.out.println(Calculator.divide(num1,num2));
         System.out.println(Calculator.multiply(num1,num2));
+
+        logger.info("It is an info logger");
+        logger.error("It is an error logger");
+
     }
 }
